5673989e09cca793f13d846c28018889d9cb79bf
Innovator <shahriar.firoz@gmail.com>
2019-01-26 07:00:30 -0600

    Adding components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { DashBoard } from './components/DashBoard';
+import { Highlight } from './components/Highlight';
+import { BookList } from './components/BookList';
+import { Favorites } from './components/Favorites';
+import { Menu } from './components/Menu';
+import idb from 'idb';
+import axios from 'axios';
+
 
 class App extends Component {
   render() {
